0d0d479cdfe913e7a711a503212a051c1a5c70ce
nswank1901 <93402347+nswank1901@users.noreply.github.com>
2023-10-02 19:07:40 -0400

    Added an alternate print statement.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Here is another print statement.");
+		System.out.println("Here is another alternate print statement.");
 	}
 
 }
